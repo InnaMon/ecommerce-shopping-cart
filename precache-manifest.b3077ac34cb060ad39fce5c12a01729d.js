7172453337234366eb5834a44fdf521626f4a755
Inna Mon <inna.mon@gmail.com>
2019-10-24 20:33:53 -0500

    Updates

---
+++ precache-manifest.b3077ac34cb060ad39fce5c12a01729d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0c85f0265438486dceb60f7388d9b5",
+    "url": "/ecommerce-shopping-cart/index.html"
+  },
+  {
+    "revision": "9c1c9b28ba74a012775b",
+    "url": "/ecommerce-shopping-cart/static/css/main.d47501a0.chunk.css"
+  },
+  {
+    "revision": "153b4d86baf2cdc75cb3",
+    "url": "/ecommerce-shopping-cart/static/js/2.b59837b3.chunk.js"
+  },
+  {
+    "revision": "9c1c9b28ba74a012775b",
+    "url": "/ecommerce-shopping-cart/static/js/main.5a980836.chunk.js"
+  },
+  {
+    "revision": "cc0c1a9c622766146c20",
+    "url": "/ecommerce-shopping-cart/static/js/runtime-main.4baf2ee2.js"
+  }
+]);
